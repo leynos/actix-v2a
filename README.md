6b2f3e8b0155bc11e06e5764963b1f70b77cc28b
Payton McIntosh <pmcintosh@df12.net>
2026-04-02 07:51:55 +0100

    Add repo skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Actix v2a components
+
+This is a generated project using [Copier](https://copier.readthedocs.io/).
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! `Actix v2a components` library.
+
+// TODO: Remove this stub and implement actual library functionality.
+/// Returns a greeting for the library.
+#[must_use]
+pub const fn greet() -> &'static str {
+    "Hello from Actix v2a components!"
+}
